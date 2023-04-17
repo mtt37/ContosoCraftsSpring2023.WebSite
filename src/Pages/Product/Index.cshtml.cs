da24b9cef7f0316f657b909c7b9a6b61e19c8fe7
mckeemseattleu <mckeem@seattleu.edu>
2023-04-17 15:51:21 -0700

    Added ProductIndex.razor for Index Page modifications

--- src/Pages/Product/Index.cshtml.cs
+++ src/Pages/Product/Index.cshtml.cs
@@ -1,27 +1,34 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
 using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Services;
 
-namespace ContosoCrafts.WebSite.Pages
+namespace ContosoCrafts.WebSite.Pages.Product
 {
+    /// <summary>
+    /// Index Page will return all the data to show
+    /// </summary>
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger,
-            JsonFileProductService productService)
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public IndexModel(JsonFileProductService productService)
         {
-            _logger = logger;
             ProductService = productService;
         }
 
+        // Data Service
         public JsonFileProductService ProductService { get; }
+
+        // Collection of the Data
         public IEnumerable<ProductModel> Products { get; private set; }
 
+        /// <summary>
+        /// REST OnGet, return all data
+        /// </summary>
         public void OnGet()
         {
             Products = ProductService.GetAllData();
