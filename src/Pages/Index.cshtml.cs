992a09f0cb8c741332b6ae9255775f9ccf159d12
mckeemseattleu <mckeem@seattleu.edu>
2023-04-18 09:55:05 -0700

    Changed Index.cshtml to put "Index" into it's Viewdata.

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -24,7 +24,7 @@ namespace ContosoCrafts.WebSite.Pages
 
         public void OnGet()
         {
-            Products = ProductService.GetProducts();
+            Products = ProductService.GetAllData();
         }
     }
 }
