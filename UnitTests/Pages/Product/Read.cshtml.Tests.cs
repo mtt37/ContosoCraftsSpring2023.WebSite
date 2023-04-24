c824aaaf21ee5a9949a0d06065ac37b649de62e9
mckeemseattleu <mckeem@seattleu.edu>
2023-04-24 10:16:32 -0700

    Added UnitTest for Read Razor Page called Read.cshtml.Tests.cs

---
+++ UnitTests/Pages/Product/Read.cshtml.Tests.cs
@@ -0,0 +1,38 @@
+﻿
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages.Product;
+
+namespace UnitTests.Pages.Product.Read
+{
+    public class ReadTests
+    {
+        #region TestSetup
+        public static ReadModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new ReadModel(TestHelper.ProductService)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Should_Return_Products()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet("jenlooper-cactus");
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual("The Quantified Cactus: An Easy Plant Soil Moisture Sensor", pageModel.Product.Title);
+        }
+        #endregion OnGet
+    }
+}
