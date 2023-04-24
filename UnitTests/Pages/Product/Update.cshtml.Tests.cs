8fcc2943fdc45ad63bb425d02555f4bcc9df4cc3
mckeemseattleu <mckeem@seattleu.edu>
2023-04-24 11:27:47 -0700

    Refactored to use the TestHelper in Update.cshtml.Tests.cs

--- UnitTests/Pages/Product/Update.cshtml.Tests.cs
+++ UnitTests/Pages/Product/Update.cshtml.Tests.cs
@@ -1,20 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Routing;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
-using Microsoft.AspNetCore.Mvc.ViewFeatures;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Routing;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Logging;
-
-using Moq;
 
 using NUnit.Framework;
 
 using ContosoCrafts.WebSite.Pages.Product;
-using ContosoCrafts.WebSite.Services;
-using System.Threading.Tasks;
 using ContosoCrafts.WebSite.Models;
 
 namespace UnitTests.Pages.Product.Update
@@ -22,50 +11,12 @@ namespace UnitTests.Pages.Product.Update
     public class UpdateTests
     {
         #region TestSetup
-        public static IUrlHelperFactory urlHelperFactory;
-        public static DefaultHttpContext httpContextDefault;
-        public static IWebHostEnvironment webHostEnvironment;
-        public static ModelStateDictionary modelState;
-        public static ActionContext actionContext;
-        public static EmptyModelMetadataProvider modelMetadataProvider;
-        public static ViewDataDictionary viewData;
-        public static TempDataDictionary tempData;
-        public static PageContext pageContext;
-
         public static UpdateModel pageModel;
 
         [SetUp]
         public void TestInitialize()
         {
-            httpContextDefault = new DefaultHttpContext()
-            {
-                //RequestServices = serviceProviderMock.Object,
-            };
-
-            modelState = new ModelStateDictionary();
-
-            actionContext = new ActionContext(httpContextDefault, httpContextDefault.GetRouteData(), new PageActionDescriptor(), modelState);
-
-            modelMetadataProvider = new EmptyModelMetadataProvider();
-            viewData = new ViewDataDictionary(modelMetadataProvider, modelState);
-            tempData = new TempDataDictionary(httpContextDefault, Mock.Of<ITempDataProvider>());
-
-            pageContext = new PageContext(actionContext)
-            {
-                ViewData = viewData,
-            };
-
-            var mockWebHostEnvironment = new Mock<IWebHostEnvironment>();
-            mockWebHostEnvironment.Setup(m => m.EnvironmentName).Returns("Hosting:UnitTestEnvironment");
-            mockWebHostEnvironment.Setup(m => m.WebRootPath).Returns("../../../../src/bin/Debug/net6.0/wwwroot");
-            mockWebHostEnvironment.Setup(m => m.ContentRootPath).Returns("./data/");
-
-            var MockLoggerDirect = Mock.Of<ILogger<IndexModel>>();
-            JsonFileProductService productService;
-
-            productService = new JsonFileProductService(mockWebHostEnvironment.Object);
-
-            pageModel = new UpdateModel(productService)
+            pageModel = new UpdateModel(TestHelper.ProductService)
             {
             };
         }
@@ -79,22 +30,22 @@ namespace UnitTests.Pages.Product.Update
             // Arrange
 
             // Act
-            pageModel.OnGet("jenlooper-cactus");
+            pageModel.OnGet("selinazawacki-shirt");
 
             // Assert
             Assert.AreEqual(true, pageModel.ModelState.IsValid);
-            Assert.AreEqual("The Quantified Cactus: An Easy Plant Soil Moisture Sensor", pageModel.Product.Title);
+            Assert.AreEqual("Floppy Crop", pageModel.Product.Title);
         }
         #endregion OnGet
 
-        #region OnPostAsync
+        #region OnPost
         [Test]
-        public void OnPostAsync_Valid_Should_Return_Products()
+        public void OnPost_Valid_Should_Return_Products()
         {
             // Arrange
             pageModel.Product = new ProductModel
             {
-                Id = "jenlooper-cactus",
+                Id = "selinazawacki-moon",
                 Title = "title",
                 Description = "description",
                 Url = "url",
@@ -110,17 +61,9 @@ namespace UnitTests.Pages.Product.Update
         }
 
         [Test]
-        public void OnPostAsync_InValid_Model_NotValid_Return_Page()
+        public void OnPost_InValid_Model_NotValid_Return_Page()
         {
             // Arrange
-            pageModel.Product = new ProductModel
-            {
-                Id = "jenlooper-cactus",
-                Title = "title",
-                Description = "description",
-                Url = "url",
-                Image = "image"
-            };
 
             // Force an invalid error state
             pageModel.ModelState.AddModelError("bogus", "bogus error");
@@ -131,6 +74,6 @@ namespace UnitTests.Pages.Product.Update
             // Assert
             Assert.AreEqual(false, pageModel.ModelState.IsValid);
         }
-        #endregion OnPostAsync
+        #endregion OnPost
     }
 }
