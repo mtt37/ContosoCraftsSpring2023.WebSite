33531340b0a5fa69bd41fa6bebc6e53cd0c823c7
mckeemseattleu <mckeem@seattleu.edu>
2023-04-24 10:24:49 -0700

    Added Razor page (Updaste.cshtml, Update.cshtml.cs) to project

---
+++ src/Pages/Product/Update.cshtml.cs
@@ -0,0 +1,52 @@
+using System.Linq;
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoCrafts.WebSite.Pages.Product
+{
+    public class UpdateModel : PageModel
+    {
+        // Data middletier
+        public JsonFileProductService ProductService { get; }
+
+        /// <summary>
+        /// Defualt Construtor
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="productService"></param>
+        public UpdateModel(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        // The data to show, bind to it for the post
+        [BindProperty]
+        public ProductModel Product { get; set; }
+
+        /// <summary>
+        /// REST Get request
+        /// Loads the Data
+        /// </summary>
+        /// <param name="id"></param>
+        public void OnGet(string id)
+        {
+            Product = ProductService.GetAllData().FirstOrDefault(m => m.Id.Equals(id));
+        }
+
+        public IActionResult OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            ProductService.UpdateData(Product);
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
