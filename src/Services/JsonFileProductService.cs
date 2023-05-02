ca8afb6ff25f45eac3b46ded32cf4d21e8efa59c
mckeemseattleu <mckeem@seattleu.edu>
2023-05-01 18:21:05 -0700

    Trim the Description in JsonFileProductService.cs

--- src/Services/JsonFileProductService.cs
+++ src/Services/JsonFileProductService.cs
@@ -108,7 +108,7 @@ namespace ContosoCrafts.WebSite.Services
             }
             // Update the data to the new passed in values
             productData.Title = data.Title;
-            productData.Description = data.Description;
+            productData.Description = data.Description.Trim();
             productData.Url = data.Url;
             productData.Image = data.Image;
             productData.Quantity = data.Quantity;
