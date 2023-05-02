ffba25a38a0ec6d473fd34da758ddf93cd111f68
mckeemseattleu <mckeem@seattleu.edu>
2023-05-01 18:13:01 -0700

    Added attribute Quantity to ProductModel.cs

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -1,6 +1,6 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
-
+using System.ComponentModel.DataAnnotations;
 namespace ContosoCrafts.WebSite.Models
 {
     public class ProductModel
@@ -14,6 +14,12 @@ namespace ContosoCrafts.WebSite.Models
         public string Title { get; set; }
         public string Description { get; set; }
         public int[] Ratings { get; set; }
+        // Quanitity contains the amount
+        public string Quantity { get; set; }
+        // Defensive Coding for price
+        [Range(-1, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public int Price { get; set; }
+
 
         public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
 
